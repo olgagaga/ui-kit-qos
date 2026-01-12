cd27f98123562a7307a61146ed4c713fae66b05a
olgagaga <olga_kuzmich2005@tut.by>
2026-01-12 23:59:53 +0300

    assignment icon size flexibility added

--- src/components/icons/components/Assignment.vue
+++ src/components/icons/components/Assignment.vue
@@ -1,7 +1,8 @@
 <template>
   <svg
-    width="24"
-    height="24"
+    :width="size"
+    :height="size"
+    :class="className"
     viewBox="0 0 16 16"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
